a3b42fdc64a7339044fb9013204f03eafffd0f14
GURUPRASAD K MURTHY <72002243+monu301981@users.noreply.github.com>
2025-03-19 19:57:33 +0530

    Adding notebook open option for PHP.
    Adding notebook open option for PHP.

---
+++ php/php-jupyter-open.bat
@@ -0,0 +1,9 @@
+@echo off
+for /f "delims=" %%a in (../default.config) do set %%a
+
+@echo on
+:: Windows
+docker run --rm -p 10003:8888 -v %workspace%\%workfolder%:/home/jovyan/work benramsey/jupyter-php:latest
+:: Linux
+:: docker run --rm -p 8888:8888 -v "$PWD":/home/jovyan/work benramsey/jupyter-php:latest
+
