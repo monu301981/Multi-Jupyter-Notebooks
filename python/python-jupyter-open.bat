ee4581e87c1f7ecf4b9d696f0f4f549c27d86eeb
GURUPRASAD K MURTHY <72002243+monu301981@users.noreply.github.com>
2025-03-19 19:56:20 +0530

    Adding notebook open option for Python.
    Adding notebook open option for Python.

---
+++ python/python-jupyter-open.bat
@@ -0,0 +1,10 @@
+@echo off
+for /f "delims=" %%a in (../default.config) do set %%a
+
+@echo on
+:: Windows
+docker run -it --rm -p 10000:8888 -v %workspace%\%workfolder%:/home/jovyan/work jupyter/minimal-notebook
+:: Linux
+:: docker run --rm -p 8888:8888 -v "$PWD":/home/jovyan/work jupyter/minimal-notebook
+
+ 
