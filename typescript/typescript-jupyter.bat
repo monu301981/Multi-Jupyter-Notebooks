5cafb28871673a44c87c443b35ce11a25c7d2a71
GURUPRASAD K MURTHY <72002243+monu301981@users.noreply.github.com>
2025-03-21 12:50:16 +0530

    Adding support for Typescript and R languages
    Adding support for Typescript and R languages

---
+++ typescript/typescript-jupyter.bat
@@ -0,0 +1,8 @@
+@echo off
+for /f "delims=" %%a in (../default.config) do set %%a
+
+@echo on
+docker pull jupyter/base-notebook
+docker run -it --rm -p 10004:8888 -v %workspace%\%workfolder%:/home/jovyan/work jupyter/base-notebook sh -c "apt-get update; apt-get -y install node;apt-get -y install npm; npm install -g deno ; deno jupyter --install;jupyter lab;"
+:: Linux
+:: docker run --rm -p 8888:8888 -v "$PWD":/home/jovyan/work jupyter/base-notebook sh -c "apt-get update; apt-get -y install node;apt-get -y install npm; npm install -g deno ; deno jupyter --install;jupyter lab;"
